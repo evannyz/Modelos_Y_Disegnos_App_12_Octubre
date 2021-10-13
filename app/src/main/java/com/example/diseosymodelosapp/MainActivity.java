43605b016d668bb9b76310955b39ffa99b4b6fe2
evans <evanny.zapata@gmail.com>
2021-10-13 19:56:00 -0300

    Se realiza el mismo ejercicio de Picasso con la libreria Glide

--- app/src/main/java/com/example/diseosymodelosapp/MainActivity.java
+++ app/src/main/java/com/example/diseosymodelosapp/MainActivity.java
@@ -5,32 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView iv_avatar;
+    private ImageView ivAvatarSuperior,ivAvatarInferior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         initWidgets();
         loadImage();
+        loadImageGlide();
 
 
 
     }
 
+    private void loadImageGlide() {
+        Glide.with(this)
+                .load("https://smallimg.pngkey.com/png/small/127-1271587_one-piece-v2-icon-anime-one-piece.png")
+                .into(ivAvatarInferior);
+    }
+
     private void initWidgets() {
-        iv_avatar=findViewById(R.id.iv_logo);
+        ivAvatarSuperior =findViewById(R.id.iv_logo);
+        ivAvatarInferior =findViewById(R.id.iv_logo_inferior);
     }
 
     private void loadImage() {
         Picasso.get()
                 .load("https://e7.pngegg.com/pngimages/462/369/png-clipart-uzumaki-naruto-naruto-throwing-ball-comics-and-fantasy-naruto.png")
-                .into(iv_avatar);
+                .into(ivAvatarSuperior);
 
 
 
