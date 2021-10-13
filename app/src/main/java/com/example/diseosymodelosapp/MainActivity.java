d7d3c7ac2133c654bbc069d53b83b9243de5c339
evans <evanny.zapata@gmail.com>
2021-10-13 00:44:27 -0300

    Se añade la libreria picaso desde la siguiente url: https://github.com/square/picasso se configura en build.gradle de la app se usa la implementacion  implementation 'com.squareup.picasso:picasso:2.71828' luego en manifest se añade el permiso de internet <uses-permission android:name=android.permission.INTERNET/> y desde una imagen png desde main java se crea una funcion para poder cargar en un image view desde una url con las opciones de la libreria Picasso.get().load(url_png_de_naruto).into(image_view_declarado)

--- app/src/main/java/com/example/diseosymodelosapp/MainActivity.java
+++ app/src/main/java/com/example/diseosymodelosapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.diseosymodelosapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidgets();
+        loadImage();
+
+
+
+    }
+
+    private void initWidgets() {
+        iv_avatar=findViewById(R.id.iv_logo);
+    }
+
+    private void loadImage() {
+        Picasso.get()
+                .load("https://e7.pngegg.com/pngimages/462/369/png-clipart-uzumaki-naruto-naruto-throwing-ball-comics-and-fantasy-naruto.png")
+                .into(iv_avatar);
+
+
+
     }
 }
